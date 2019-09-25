00b3af528a232645bdf1c75484967a4623151d85
Viktor Orekoya <oorekoya@telensa.com>
2019-09-25 15:24:01 +0100

    relocates data files and includes ssh keys

--- custom/submit/spark-submit.sh
+++ custom/submit/spark-submit.sh
@@ -1,16 +1,24 @@
  #!/bin/bash
  
- ls -lath /opt/spark-data/;
+ENV DEPLOY_MODE client
+ENV EXEC_CORES 1
+
+echo "list files in spark-data"
+
+ls -lath /opt/spark-data/;
 
 echo "********************"
+echo "list files in spark-apps"
 
- ls -lath /opt/spark-apps/;
+ls -lath /opt/spark-apps/;
+
+printenv;
 
 /spark/bin/spark-submit \
 --class ${SPARK_APPLICATION_MAIN_CLASS} \
 --master ${SPARK_MASTER_URL} \
---deploy-mode client \
---total-executor-cores 1 \
+--deploy-mode ${DEPLOY_MODE} \
+--total-executor-cores ${EXEC_CORES} \
  ${SPARK_SUBMIT_ARGS} \
  ${SPARK_APPLICATION_JAR_LOCATION} \
  ${SPARK_APPLICATION_ARGS} 
